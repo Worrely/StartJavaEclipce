20195ce4a5a60ceda4c36a87fe8235129412cb65
Worrely <69391817+Worrely@users.noreply.github.com>
2022-02-02 10:50:05 +0400

    Add files via upload

--- src/by/unit01/main/Start.java
+++ src/by/unit01/main/Start.java
@@ -5,7 +5,21 @@ public class Start {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, World!!!");
-		Second.method();
+		Second.method();  //добавим метод из другого места(файла)
+		
+		//Вычислить периметр и площадь прямоугольного треугольника по длинам а и b двух катетов.
+		double a, b; // два катета
+		double c = 0; // гипотенуза
+		double p; // периметр
+		double s; // площадь
+		a = 10;
+		b = 20;
+		c = Math.sqrt(a * a + b * b);
+		p = a + b + c;
+		s = 0.5 * a * b;
+		System.out.println("Периметр = " + p + " площадь = " + s);
+		
+		
 		
 	}
 	
