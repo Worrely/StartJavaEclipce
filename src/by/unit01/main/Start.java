3c48907a12d27d72aedec614d3fa76d472db8f4f
Worrely <69391817+Worrely@users.noreply.github.com>
2022-02-02 10:57:32 +0400

    Add files via upload

--- src/by/unit01/main/Start.java
+++ src/by/unit01/main/Start.java
@@ -17,7 +17,23 @@ public class Start {
 		c = Math.sqrt(a * a + b * b);
 		p = a + b + c;
 		s = 0.5 * a * b;
+		System.out.println("Вычислить периметр и площадь прямоугольного треугольника");
 		System.out.println("Периметр = " + p + " площадь = " + s);
+		
+		//Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+		double x = 0, y = 0, z;
+		x = 10.2;
+		y = 13.6;
+		double temp;
+		temp = Math.cos(x) - x / 3;
+		if (temp != 0) {
+		z = x * Math.log(x) + y / temp;
+		} else {
+		System.out.println("Знаменатель равен нулю.");
+		z = Double.NaN;
+		}
+		System.out.println("Вычислить значение выражения по формуле");
+		System.out.println("z=" + z);
 		
 		
 		
